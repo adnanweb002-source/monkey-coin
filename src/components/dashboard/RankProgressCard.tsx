1cfa79f5a042dd51cd76adac50be85a51c5775e9
adnanweb002-source <adnan.web002@gmail.com>
2026-03-24 02:57:11 +0530

    [Fix]: Rank progress card

--- src/components/dashboard/RankProgressCard.tsx
+++ src/components/dashboard/RankProgressCard.tsx
@@ -53,7 +53,7 @@ const RankProgressCard = () => {
   const currentRank = ranks?.filter((r) => r.claimed)?.slice(-1)[0];
 
   // 🧠 Compute next rank
-  const nextRanks = ranks?.filter((r) => r.claimable);
+  const nextRanks = ranks?.filter((r) => r.order > currentRank.order);
 
   const nextRankObj = nextRanks?.find((r)=> r.order > currentRank.order);
 

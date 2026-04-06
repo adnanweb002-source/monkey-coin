a29736565a432cba86465d263f9ea081d03477da
adnanweb002 <adnan.web002@gmail.com>
2026-04-06 23:02:16 +0530

    [Fix]: login redirection urls

--- src/App.tsx
+++ src/App.tsx
@@ -75,6 +75,11 @@ const App = () => (
         <Sonner />
         <BrowserRouter basename="/panel">
           <Routes>
+
+          <Route path="/" element={<ProtectedDashboard />}>
+              <Route index element={<DashboardHome />} />
+            </Route>
+
             <Route path="/signup" element={<Index />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
